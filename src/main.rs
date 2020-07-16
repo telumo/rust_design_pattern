e0881d2f0b6aee171ffb5362feea71d2b933f77e
telumo <drumscohika@gmail.com>
2020-07-16 14:14:43 +0900

    シングルトン完了

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ mod adapter;
 mod factory_method;
 mod iterator;
 mod template_method;
+mod singleton;
 
 fn main() {
     iterator::run();
     adapter::run();
     template_method::run();
     factory_method::run();
+    singleton::run();
 }
---
+++ src/singleton/mod.rs
@@ -0,0 +1,7 @@
+
+
+pub fn run() {
+    // シングルトンはやりません。
+    // see: https://rust-embedded.github.io/book/peripherals/singletons.html
+    println!("Yeah!! Singleton!")
+}
